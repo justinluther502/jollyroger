1b579a5f8f3766b7be5cd39521b14dbbab5ef060
Justin Luther <justinluther@lutherwealth.com>
2021-01-08 12:27:38 -0500

    added landing page modal for demo instructions.

---
+++ src/components/LandingPage/DemoInstructions.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <p>
+      Thanks for helping to improve Indie Adviser! Please keep a few things in
+      mind while testing it out:
+    </p>
+    <ul>
+      <li>
+        Several of the pages require user authentication. Click "Sign In" at
+        the top right of this screen to log in. There is a temporary set of
+        dummy credentials with fake info listed at the bottom of the login
+        page for testing purposes. If you are having problems viewing pages
+        try signing out and logging back in.
+      </li>
+      <li>
+        The search bar doesn't do anything yet.
+      </li>
+      <li>
+        If you are an actual financial adviser that uses Interactive Brokers
+        for brokerage and custodian services, you can create a temporary free
+        testing account. Just attach your IBKR flex-web-query token to your
+        Indie Adviser account during account creation.
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DemoInstructions.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/LandingPage/LandingPage.vue
+++ src/components/LandingPage/LandingPage.vue
@@ -1,5 +1,9 @@
 <template>
   <main role="main" class="container-fluid">
+    <b-modal id="demo-modal" title="Welcome to the Indie Adviser demo"
+             size="lg">
+      <demo-instructions />
+    </b-modal>
     <carousel/>
     <hr>
     <landing-sections/>
@@ -12,10 +16,14 @@
 <script>
 import Carousel from "@/components/LandingPage/Carousel"
 import LandingSections from "@/components/LandingPage/LandingSections"
+import DemoInstructions from "@/components/LandingPage/DemoInstructions"
 
 export default {
   name: "LandingPage",
-  components: {Carousel, LandingSections}
+  components: {Carousel, LandingSections, DemoInstructions},
+  mounted() {
+    this.$bvModal.show('demo-modal')
+  }
 }
 </script>
 
