ccf751f2981e0510b37b87979e6583dba253dfb4
Justin Luther <justinluther@lutherwealth.com>
2021-01-09 12:04:12 -0500

    fixed favicon and app title to Indie Adviser

--- vue.config.js
+++ vue.config.js
@@ -8,5 +8,14 @@ module.exports = {
         ws: true,
       },
     }
-  }
+  },
+  // pwa: {
+  //   iconPaths: {
+  //     favicon32: "./public/favicon.ico",
+  //     favicon16: "./public/favicon.ico",
+  //     appleTouchIcon: "./public/favicon.ico",
+  //     maskIcon: "./public/favicon.ico",
+  //     msTileImage: "./public/favicon.ico"
+  //   }
+  // }
 }
