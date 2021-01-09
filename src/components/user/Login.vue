48c88114ce6640387acb5f97ddb73f5e0b85dab5
Justin Luther <justinluther@lutherwealth.com>
2021-01-08 22:34:21 -0500

    styling on login page.

--- src/components/user/Login.vue
+++ src/components/user/Login.vue
@@ -1,41 +1,52 @@
 <template>
-  <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
+  <main role="main" class="container">
     <h2>Adviser Login</h2>
-    <form>
-      <div class="form-group">
-        <label>User name</label>
-        <input class="form-control"
-               v-model="username" type="text" placeholder="username"/>
-        <label>Password</label>
-        <input class="form-control"
-               v-model="password" type="password" placeholder="password"/>
-        <br>
-        <button class="btn btn-primary" v-on:click="loginUser">Login</button>
+    <div class="row">
+      <div class="col-6">
+        <div class="form-group">
+          <label>User name</label>
+          <input class="form-control"
+                 v-model="username" type="text" placeholder="username"/>
+          <label>Password</label>
+          <input class="form-control"
+                 v-model="password" type="password" placeholder="password"/>
+          <br>
+        </div>
       </div>
-    </form>
+    </div>
+    <div class="row">
+      <div class="col-6">
+          <b-button v-on:click="loginUser" class="mr-1" variant="primary">
+            Login
+          </b-button>
+          <b-button :to="loginUser" class="mr-1" variant="success">
+            Register as New Adviser
+          </b-button>
+      </div>
+    </div>
   </main>
 </template>
 
 <script>
-import { AUTH_REQUEST } from "@/store/actions/auth"
+import {AUTH_REQUEST} from "@/store/actions/auth"
 
 export default {
   name: "Login.vue",
-  data () {
+  data() {
     return {
       username: "",
       password: ""
     }
   },
   methods: {
-    loginUser () {
+    loginUser() {
       const username = this.username
       const password = this.password
       console.log('logging in...')
-      this.$store.dispatch(AUTH_REQUEST, { username, password })
-        .then(
-            this.$router.push('/')
-        )
+      this.$store.dispatch(AUTH_REQUEST, {username, password})
+          .then(
+              this.$router.push('/')
+          )
     }
   }
 }
