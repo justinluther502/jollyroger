763429343086a866e72027ee64174b7c01fe17fc
Justin Luther <justinluther@lutherwealth.com>
2021-01-17 23:18:49 -0500

    styling and better error handling on login page.

--- src/components/user/Login.vue
+++ src/components/user/Login.vue
@@ -2,12 +2,12 @@
   <main role="main" class="container">
     <h2>Adviser Login</h2>
     <div class="row">
-      <div class="col-6">
+      <div class="col col-md-6">
         <div class="form-group">
-          <label>User name</label>
+          <label class="font-weight-bold">User name</label>
           <input class="form-control"
                  v-model="username" type="text" placeholder="username"/>
-          <label>Password</label>
+          <label class="mt-2 font-weight-bold">Password</label>
           <input class="form-control"
                  v-model="password" type="password" placeholder="password"/>
           <br>
@@ -15,13 +15,23 @@
       </div>
     </div>
     <div class="row">
-      <div class="col-6">
-        <b-button v-on:click="loginUser" class="mr-1" variant="primary">
+      <div class="col col-12 col-md-6">
+        <b-button block v-on:click="loginUser" class="mr-1" variant="primary">
           Login
         </b-button>
-        <b-button :to="{ name: 'register' }" class="mr-1" variant="success">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col col-12 col-md-6">
+        <b-button block :to="{ name: 'register' }" class="mr-1 mt-1"
+                  variant="success">
           Register as New Adviser
         </b-button>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col mt-2">
+        <b-spinner v-if="waiting" variant="primary"/>
       </div>
     </div>
   </main>
@@ -35,16 +45,50 @@ export default {
   data() {
     return {
       username: "",
-      password: ""
+      password: "",
+      waiting: false,
+      errormsg: ""
     }
   },
   methods: {
     loginUser() {
+      this.waiting = true
       const username = this.username
       const password = this.password
       this.$store.dispatch(AUTH_REQUEST, {username, password})
           .then(() => {
+            this.waiting = false
             this.$router.push('/')
+          })
+          .catch((error) => {
+            // handle error
+            this.waiting = false
+            if (error.response) {
+              // Request made and server responded
+              if (error.response.status === 400) {
+                this.errormsg = "Sorry, we didn't recognize that username / " +
+                    "password."
+                this.$bvToast.toast(this.errormsg, {
+                  title: '400 - Bad Request',
+                  toaster: 'b-toaster-top-center',
+                  autoHideDelay: 3000
+                })
+              } else {
+                this.errormsg = error.response.statusMessage
+                this.$bvToast.toast(this.errormsg, {
+                  title: error.response.statusCode,
+                  toaster: 'b-toaster-top-center',
+                  autoHideDelay: 3000
+                })
+              }
+            } else if (error.request) {
+              // The request was made but no response was received
+              console.log(error.request)
+            } else {
+              // Something happened in setting up the request that triggered
+              // an Error
+              console.log('Error', error.message)
+            }
           })
     }
   }
